5890f3c0804f7355e5efb096a9fd8f2c50926ce7
Roux13 <u.nehodov@gmail.com>
2019-06-29 01:16:16 +0300

    test FieldTest - updated

--- test/my/xo/game/model/FieldTest.java
+++ test/my/xo/game/model/FieldTest.java
@@ -1,5 +1,7 @@
 package my.xo.game.model;
 
+import my.xo.game.model.exceptions.AllreadyOccupiedEcxeption;
+import my.xo.game.model.exceptions.InvalidPointException;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,10 +23,105 @@ class FieldTest {
         final Figure inputFigure = Figure.O;
         final Figure expectedFigure = inputFigure;
 
-        field.setFigure(inputPoint, inputFigure);
+        try {
+            field.setFigure(inputPoint, inputFigure);
+
+            final Figure actualFigure = field.getFigure(inputPoint);
+
+            assertEquals(expectedFigure, actualFigure);
+        }
+        catch (AllreadyOccupiedEcxeption e) {
+
+        }
+        catch (InvalidPointException e) {
+
+        }
+    }
+
+    @Test
+    void testGetFigureWhenFigureIsNotSet() throws Exception{
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, 0);
 
         final Figure actualFigure = field.getFigure(inputPoint);
 
-        assertEquals(expectedFigure, actualFigure);
+        assertNull(actualFigure);
+    }
+
+    @Test
+    void testGetFigureWhenXIsNegative() {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1, 0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        }
+        catch (final InvalidPointException e) {
+        }
+    }
+
+    @Test
+    void testGetFigureWhenYIsNegative() {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, -1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        }
+        catch (final InvalidPointException e) {
+        }
+    }
+    @Test
+    void testGetFigureWhenXIsBigger() {
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSIZE() + 1, 0);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        }
+        catch (final InvalidPointException e) {
+        }
+    }
+
+    @Test
+    void testGetFigureWhenYIsBigger() {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0, field.getSIZE() + 1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        }
+        catch (final InvalidPointException e) {
+        }
+    }
+
+    @Test
+    void testGetFigureWhenXAndYIsNegative() {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1, -1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        }
+        catch (final InvalidPointException e) {
+        }
+    }
+
+    @Test
+    void testGetFigureWhenXAndYIsBigger() {
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSIZE() + 1, field.getSIZE() + 1);
+
+        try {
+            field.getFigure(inputPoint);
+            fail();
+        }
+        catch (final InvalidPointException e) {
+        }
     }
 }
