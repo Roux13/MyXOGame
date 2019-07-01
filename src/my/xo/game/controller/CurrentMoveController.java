4eba5e3bde8e26bd6f194cdc64ef44764856cb75
Roux <nehren@rabler.ru>
2019-07-01 20:06:39 +0300

    CurrentMoveController refactored. counts "X" and "Y" replaced by countFigure.

--- src/my/xo/game/controller/CurrentMoveController.java
+++ src/my/xo/game/controller/CurrentMoveController.java
@@ -8,26 +8,19 @@ import my.xo.game.model.exceptions.InvalidPointException;
 public class CurrentMoveController {
 
     public Figure currentMove(final Field field) throws InvalidPointException {
-        int countX = 0;
-        int countO = 0;
+        int countFIgure = 0;
         for (int i = 0; i < field.getSize(); i++) {
             for (int j = 0; j < field.getSize(); j++) {
-                Point point = new Point(i, j);
-                if (field.getFigure(point) == null) {
-                    continue;
-                }
-                if (field.getFigure(point).equals(Figure.X)) {
-                    countX++;
-                }
-                if (field.getFigure(point).equals(Figure.O)) {
-                    countO++;
+                final Point point = new Point(i, j);
+                if (field.getFigure(point) != null) {
+                    countFIgure++;
                 }
             }
         }
-        if (countO + countX == field.getSize() * field.getSize()) {
+        if (countFIgure == field.getSize() * field.getSize()) {
             return null;
         }
-        else if (countX == countO) {
+        else if (countFIgure % 2 == 0) {
             return Figure.X;
         }
         else  {
